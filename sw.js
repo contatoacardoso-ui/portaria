093e9cb64ce0ee7631d88bfc3902355755ddd9bb
contatoacardoso-ui <contato.acardoso@gmail.com>
2026-03-23 21:02:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,24 @@
-const CACHE_NAME = 'portaria-v1';
-const assets = [
+const CACHE_NAME = 'encomendas-adm-v1';
+// Arquivos que serão salvos para funcionar offline
+const assetsToCache = [
   './',
   './index.html',
   './manifest.json',
   'https://unpkg.com/html5-qrcode'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
