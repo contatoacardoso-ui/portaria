0e352ab7aa91c2abe8b89fe1d07993e8c3da64e6
contatoacardoso-ui <contato.acardoso@gmail.com>
2026-03-24 12:26:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = 'portaria-cache-v3';
+const CACHE_NAME = 'portaria-pro-v5-8';
+
 const assetsToCache = [
   './',
   './index.html',
   './manifest.json',
   'https://unpkg.com/html5-qrcode/html5-qrcode.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js',
+  'https://cdn.jsdelivr.net/npm/tesseract.js@4.0.2/dist/tesseract.min.js'
 ];
 
 self.addEventListener('install', (event) => {
@@ -17,10 +19,17 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
-    ))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
