1b0789c3fd3391b867a75043ce1cc80464166b15
contatoacardoso-ui <contato.acardoso@gmail.com>
2026-03-25 02:31:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protocolo-v38';
+const CACHE_NAME = 'protocolo-v39';
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))));
 self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
