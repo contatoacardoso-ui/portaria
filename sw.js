58efd45f19ddae1bc09ad8f01c4df8255dfabcb5
contatoacardoso-ui <contato.acardoso@gmail.com>
2026-03-24 23:59:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = 'protocolo-v13-historico'; 
-const assetsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://unpkg.com/html5-qrcode/html5-qrcode.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js',
-  'https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js'
-];
-
+const CACHE_NAME = 'protocolo-v17-cloud';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assetsToCache)));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))));
-  return self.clients.claim();
-});
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
