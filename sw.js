aa71f0330c717813e0d6a1043ea1e4b37b76871e
contatoacardoso-ui <contato.acardoso@gmail.com>
2026-03-25 00:09:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-const CACHE_NAME = 'protocolo-v17-cloud';
+const CACHE_NAME = 'protocolo-v18-final';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => caches.delete(k)));
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
