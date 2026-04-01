495751eef802eb4a1c964e32ac984d19955c08fe
contatoacardoso-ui <contato.acardoso@gmail.com>
2026-03-31 23:45:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,32 @@
-const CACHE_NAME = 'portaria-v93';
-const assets = ['/', 'index.html'];
+const CACHE_NAME = 'portaria-v94';
+const assets = [
+  '/',
+  'index.html',
+  'https://unpkg.com/html5-qrcode/html5-qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js'
+];
 
+// Instalação e Cache dos recursos necessários
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    );
 });
 
+// Ativação: Remove caches de versões anteriores (v93, v92...)
 self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    )));
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
 });
 
+// Estratégia de busca: Tenta rede primeiro para garantir dados atualizados do Firebase
 self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
